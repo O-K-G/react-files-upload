9833ba88b0d24138fa01c97ba44338b9c671c958
unknown <inariwind@gmail.com>
2021-09-07 20:37:06 +0300

    feature: Add file types.

--- components/handleFiles.tsx
+++ components/handleFiles.tsx
@@ -1,29 +1,33 @@
-// Allowed image file types.
-export const imageTypes = [
-  "image/png",
-  "image/svg+xml",
-  "image/jpeg",
-  "image/gif",
-  "image/bmp",
-];
+// Allowed file types.
+export const fileTypes = {
+  imageFiles: [
+    "image/png",
+    "image/svg+xml",
+    "image/jpeg",
+    "image/gif",
+    "image/bmp",
+  ],
+  documentFiles: ["application/pdf"],
 
-export const docTypes = ["application/pdf"];
-
-const handleFiles = (file: any, setLoadedFile: any) => {
-  const { type } = file; // Add "const { name, size, type } = file"; if you need the filename or filesize.
-  const blob = new Blob([file], { type: type });
-  const blobURL = URL.createObjectURL(blob);
-
-  if (imageTypes.includes(type) || docTypes.includes(type)) {
-    setLoadedFile({
-      type: type,
-      data: blobURL,
-    });
-  } else {
-    console.warn("Incorrect file type, please choose a correct file type.");
-  }
+  // unsupportedFiles: Either image or document files that aren't natively supported for display by main browsers.
+  unsupportedFiles: ["tif", "TIF", "doc", "DOC", "docx", "DOCX", "odt", "ODT"],
 };
 
-//  Add 'URL.revokeObjectURL(blobURL);' at a stage when the object isn't needed anymore.
+const handleFiles = (file: any, setLoadedFile: any) => {
+  const { name, type } = file; // Add "const { name, size, type } = file"; if you need the filesize.
+  const blob = new Blob([file], { type: type });
+  const { imageFiles, documentFiles, unsupportedFiles } = fileTypes;
+  const fileExtension = name.split(".").pop(); // Used for unsupported file extensions.
+
+  imageFiles.includes(type) ||
+  documentFiles.includes(type) ||
+  unsupportedFiles.includes(fileExtension)
+    ? setLoadedFile({
+        name: name,
+        type: type ? type : fileExtension,
+        blob: blob,
+      })
+    : console.warn("Incorrect file type, please choose a correct file type.");
+};
 
 export default handleFiles;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
 import React, { useState } from "react";
 import Image from "next/image";
-import handleFiles, { imageTypes, docTypes } from "../components/handleFiles"; // Handles the actually uploaded files.
+import handleFiles, { fileTypes } from "../components/handleFiles"; // Handles the actually uploaded files.
 
 const Home: NextPage = () => {
   const [loadedFile, setLoadedFile] = useState<any>(false);
-  const { type, data } = loadedFile;
+  const { name, type, blob } = loadedFile;
+  const { imageFiles, documentFiles, unsupportedFiles } = fileTypes;
 
   const handleUpload = (e: any) => {
     const {
@@ -16,28 +17,58 @@ const Home: NextPage = () => {
     file && handleFiles(file, setLoadedFile); // "file && ..." is to avoid code breaks when the user cancels the upload.
   };
 
+  // Simulate a fake fetch().
+  const fakeFetch = (e: any) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("firstName", "John");
+    formData.append(type, blob, name);
+    console.log(formData.getAll("firstName"));
+    console.log(formData.getAll(type));
+  };
+
+  // Add 'URL.revokeObjectURL(blobURL);' at a stage when the object isn't needed anymore.
+
   return (
     <>
-      <label htmlFor="contained-button-file">Choose a file: </label>
+      <form id="formElement" onSubmit={fakeFetch}>
+        <input type="text" name="firstName" value="Some Value" readOnly />
+        <label htmlFor="contained-button-file">Choose a file: </label>
+        <input
+          type="file"
+          id="file"
+          name="contained-button-file"
+          accept=".png, .jpg, .jpeg, .gif, .bmp, .svg, .pdf, .tif, .doc, .docx, .odt" // Changing this field also requires updating '/components/handleFiles.tsx' accordingly.
+          onChange={handleUpload}
+        />
+        <input type="submit" />
+      </form>
 
-      <input
-        type="file"
-        id="file"
-        name="contained-button-file"
-        accept=".png, .jpg, .jpeg, .gif, .bmp, .svg, .pdf" // Changing this field also requires updating '/components/handleFiles.tsx' accordingly.
-        onChange={handleUpload}
-      />
-      {imageTypes.includes(type) && (
+      {imageFiles.includes(type) && (
         <Image
           width={300}
           height={200}
-          src={data}
+          src={URL.createObjectURL(blob)}
           alt={"Some image"}
         />
       )}
 
-      {docTypes.includes(type) && (
-        <embed src={data} width="375" height="500" type={type} />
+      {documentFiles.includes(type) && (
+        <embed
+          src={URL.createObjectURL(blob)}
+          width="375"
+          height="500"
+          type={type}
+        />
+      )}
+
+      {(unsupportedFiles.includes(type) ||
+        type === "image/tiff" || // *.tif files.
+        type === "application/vnd.oasis.opendocument.text") && ( // *.odt files.
+        <p>
+          The file is loaded, but the browser does not support in natively
+          displaying it.
+        </p>
       )}
     </>
   );
